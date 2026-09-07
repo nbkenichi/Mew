nbkenichi/Mew

commit 9f3f81fd0816e77b7f48e09bd8e8c9fc9f68006e

Fix Y2038 bug in mew-time-rfc-to-sortkey
* elisp/mew-func.el (mew-time-rfc-to-sortkey): Let encode-time
tell you which years are supported, instead of assuming years past
2037 do not work.  Nowadays most platforms support years well
past 2038.

elisp/mew-func.el
@@ -1476,12 +1476,13 @@ by side-effect."
 	  (setq year (+ year 2000)))
 	 ((< year 150)
 	  (setq year (+ year 1900))))
-	(if (or (< year 1970) (>= year 2038))
-	    ;; invalid data
-	    (mew-time-ctz-to-sortkey-invalid sec min hour day mon year)
-	  (mew-time-ctz-to-sortkey (encode-time
-				    (list sec min hour day mon year
-					  nil -1 (and tzadj tmzn))))))))
+	(condition-case nil
+	    (mew-time-ctz-to-sortkey
+	     (encode-time (list sec min hour day mon year
+				nil -1 (and tzadj tmzn))))
+	  (error
+	   ;; invalid data
+	   (mew-time-ctz-to-sortkey-invalid sec min hour day mon year))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;

commit 5fd9336e13d54f1359d47c9d8bc98a27b2d368ee

fixing mew-time-rfc-to-sortkey.
This fixes the regression in 9f3f81fd0816e77b7f48e09bd8e8c9fc9f68006e.
0b0419427ecfce373a56e11272b1d2703e44599 accidentally includes the
previous fix for proof-of-concept.

mew-time-rfc-to-sortkey is used as two ways, sorting and path tracing.
The old implementation uses GMT for sorting and the local zone for
path tracing.  But the local zone can be also used for sorting. So,
new implementation uses the local zone only.

elisp/mew-func.el
@@ -1461,7 +1461,7 @@ by side-effect."
   (format "%04d%02d%02d%02d%02d%02d" year mon day hour min sec))
 
 ;; "20000726121835"
-(defun mew-time-rfc-to-sortkey (s &optional tzadj)
+(defun mew-time-rfc-to-sortkey (s)
   (if (string-match mew-time-rfc-regex s)
       (let ((year (mew-time-rfc-year))
 	    (mon  (mew-time-mon-str-to-int (mew-time-rfc-mon)))
@@ -1476,13 +1476,9 @@ by side-effect."
 	 ((< year 150)
 	  (setq year (+ year 1900))))
 	(condition-case nil
-	    ;; tmzn is used to convert the time to GMT for sorting by
-	    ;; default. If tzadj is non-nil, this is used for path
-	    ;; traceing, not for sorting. In this case, time is adjusted
-	    ;; to the local zone.
-	    (let* ((tmzn1 (if tzadj (- tmzn (car (current-time-zone))) tmzn))
-		   (tm (encode-time (list sec min hour day mon year nil -1 tmzn1))))
-	      (mew-time-ctz-to-sortkey tm))
+	    ;; This uses local zone which ensures correct behavior
+	    ;; for both sorting and path tracing.
+	    (mew-time-ctz-to-sortkey (encode-time (list sec min hour day mon year nil -1 tmzn)))
 	  (error
 	   ;; invalid data
 	   (mew-time-ctz-to-sortkey-invalid sec min hour day mon year))))))

elisp/mew-summary2.el
@@ -540,7 +540,7 @@ If called with `\\[universal-argument]', it stays writable."
 	   ;; for broken MTAs
 	   (if (string-match "Mon\\|Tue\\|Wed\\|Thu\\|Fri\\|Sat\\|Sun" ent)
 	       (setq date (substring ent (match-beginning 0)))))
-	 (if date (setq date (mew-time-rfc-to-sortkey date 'tzadj)))
+	 (if date (setq date (mew-time-rfc-to-sortkey date)))
 	 (unless date (setq date "19700101000000"))
 	 (setq tmp (cons (list date svr) tmp))
 	 (cond